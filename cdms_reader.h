9f11bfdd76dfd3edf128b344399d4c1fc31bf776
Thomas Maxwell <thomas.maxwell@nasa.gov>
2013-11-27 19:29:55 -0500

    Add header

---
+++ cdms_reader.h
@@ -0,0 +1,15 @@
+/*
+ * cdms_reader.h
+ *
+ *  Created on: Nov 27, 2013
+ *      Author: thomas
+ */
+
+#ifndef CDMS_READER_H_
+#define CDMS_READER_H_
+
+
+int cdms_test( const char* input_file );
+
+
+#endif /* CDMS_READER_H_ */
--- main.cpp
+++ main.cpp
@@ -5,14 +5,13 @@
  *      Author: tpmaxwel
  */
 
-#include "NetCDF_reader.h"
 #include "cdms_reader.h"
 #include <stdio.h>
 
 
 int main(void) {
 
-	const char* input_file = "/Developer/Data/AConaty/comp-ECMWF/ecmwf.inst3_3d_wxm_Np.20110502_00z+20110502_00z.nc";
+	const char* input_file = "~/Data/comp-ECMWF/ecmwf.inst3_3d_wxm_Np.20110502_00z+20110502_00z.nc";
 
 	int rv = cdms_test( input_file );
 
